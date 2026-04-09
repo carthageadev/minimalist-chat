1f8ea8da83b4cbbf2211a500e635f19853efc277
AhmedBenAbdallahDev <AhmedDev@email.com>
2026-04-09 18:42:12 +0100

    feat: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { SilkBackgroundAnimation } from "@/components/ui/silk-background-animation";
+
+export default function Home() {
+  return (
+    <main>
+      <SilkBackgroundAnimation />
+    </main>
+  );
+}
